00f7c31bc0cb0dcce3e02231610d16bc8123da4a
neverstopeating <tianyi.dong@gmail.com>
2021-04-16 10:50:41 +0800

    add admin list displsy

--- goal/admin.py
+++ goal/admin.py
@@ -4,6 +4,18 @@ from .models import ClockIn, SetUp, Balance
 # Register your models here.
 
 
-@admin.register(ClockIn, SetUp, Balance)
-class GoalAdmin(admin.ModelAdmin):
-    pass
+class SetUpAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'status')
+
+
+class ClockInAdmin(admin.ModelAdmin):
+    list_display = ('user', 'setup', 'image_0')
+
+
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'created_time')
+
+
+admin.site.register(SetUp, SetUpAdmin)
+admin.site.register(ClockIn, ClockInAdmin)
+admin.site.register(Balance, BalanceAdmin)
